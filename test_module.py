8afd2f022a8677ebc0c7ec9e88e00f2dd423b063
maverick12345678 <41476980+maverick12345678@users.noreply.github.com>
2020-03-31 17:22:50 +0530

    Add files via upload

---
+++ test_module.py
@@ -0,0 +1,13 @@
+import os.path
+
+
+def getssh():  # pseudo application code
+    return os.path.join(os.path.expanduser("~admin"), '.ssh')
+
+
+def test_mytest(monkeypatch):
+    def mockreturn(path):
+        return '/abc'
+    monkeypatch.setattr(os.path, 'expanduser', mockreturn)
+    x = getssh()
+    assert x == '/abc/.ssh'
